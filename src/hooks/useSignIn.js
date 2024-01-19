eba4b8d2b3e36e772fa51ae99a748d7563c2237d
Erik Peteri <peteri.erik@gmail.com>
2024-01-19 19:18:34 +0200

    exercise 1.15

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,15 +1,31 @@
-import { useMutation } from "@apollo/client";
+import { useMutation, useApolloClient } from "@apollo/client";
 import { AUTHENTICATE_USER } from "../graphql/mutations";
+import useAuthStorage from "../hooks/useAuthStorage";
+import { useNavigate } from "react-router-native";
 
 const useSignIn = () => {
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+  const navigate = useNavigate();
+
   const [mutate, result] = useMutation(AUTHENTICATE_USER);
 
   const signIn = async ({ username, password }) => {
-    const result = mutate({
-      variables: { credentials: { username: username, password: password } },
-    });
-    return result;
+    try {
+      const result = await mutate({
+        variables: { credentials: { username: username, password: password } },
+      });
+      const accessToken = result.data.authenticate.accessToken;
+      await authStorage.setAccessToken(accessToken);
+      apolloClient.resetStore();
+      navigate("/");
+
+      return result;
+    } catch (error) {
+      console.error("Incorrect credentials", error);
+    }
   };
+
   return [signIn, result];
 };
 
