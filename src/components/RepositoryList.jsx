e971a313c32e8ac6aa08e93e42e08e2ffd7169e9
Erik Peteri <peteri.erik@gmail.com>
2024-02-25 22:58:09 +0200

    exercise 10.23

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,6 +2,8 @@ import { FlatList, View, StyleSheet, Pressable } from "react-native";
 import RepositoryItem from "./RepositoryItem";
 import useRepositories from "../hooks/useRepositories";
 import { useNavigate } from "react-router-native";
+import {Picker} from '@react-native-picker/picker';
+import { useState } from "react";
 
 const styles = StyleSheet.create({
   separator: {
@@ -34,9 +36,31 @@ export const RepositoryListContainer = ({ repositories }) => {
 };
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const [selectedRepository, setselectedRepository] = useState("CREATED_AT");
+  const [direction, setdirection] = useState("DESC")
+  const [filter, setFilter] = useState("CREATED_AT")
+  const { repositories } = useRepositories({selectedRepository, direction});
 
-  return <RepositoryListContainer repositories={repositories} />;
+  return (
+    <>
+      <FlatList
+        ListHeaderComponent={<Picker
+          selectedValue={filter}
+          onValueChange={(itemValue) => {
+            setFilter(itemValue)
+            setselectedRepository(
+              itemValue === "lowest" ? "RATING_AVERAGE" : itemValue
+            );
+            setdirection(itemValue === "lowest" ? "ASC" : "DESC");
+          }}>
+          <Picker.Item label="Latest repositories" value="CREATED_AT" />
+          <Picker.Item label="Highest rated repositories" value="RATING_AVERAGE" />
+          <Picker.Item label="Lowest rated repositories" value="lowest" />
+        </Picker>}
+      />
+      <RepositoryListContainer repositories={repositories} />
+    </>
+  );
 };
 
 export default RepositoryList;
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,8 +1,9 @@
 import { useQuery } from "@apollo/client";
 import { GET_REPOSITORIES } from "../graphql/queries";
 
-const useRepositories = () => {
+const useRepositories = ({selectedRepository, direction}) => {
   const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    variables: { orderBy: selectedRepository, orderDirection: direction},
     fetchPolicy: "cache-and-network",
   });
   const repositories = data ? data.repositories : null;
