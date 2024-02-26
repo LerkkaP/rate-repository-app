f476a113b13b96ec16c49c99a3824ae4f1502fe2
Erik Peteri <peteri.erik@gmail.com>
2024-02-26 15:02:30 +0200

    exercise 10.24

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,8 +2,10 @@ import { FlatList, View, StyleSheet, Pressable } from "react-native";
 import RepositoryItem from "./RepositoryItem";
 import useRepositories from "../hooks/useRepositories";
 import { useNavigate } from "react-router-native";
-import {Picker} from '@react-native-picker/picker';
+import { Picker } from "@react-native-picker/picker";
+import { Searchbar } from "react-native-paper";
 import { useState } from "react";
+import { useDebounce } from "use-debounce";
 
 const styles = StyleSheet.create({
   separator: {
@@ -37,26 +39,48 @@ export const RepositoryListContainer = ({ repositories }) => {
 
 const RepositoryList = () => {
   const [selectedRepository, setselectedRepository] = useState("CREATED_AT");
-  const [direction, setdirection] = useState("DESC")
-  const [filter, setFilter] = useState("CREATED_AT")
-  const { repositories } = useRepositories({selectedRepository, direction});
+  const [direction, setdirection] = useState("DESC");
+  const [filter, setFilter] = useState("CREATED_AT");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [debounceSearch] = useDebounce(searchQuery, 500);
+  console.log(debounceSearch);
+  const { repositories } = useRepositories({
+    selectedRepository,
+    direction,
+    keyword: debounceSearch,
+  });
 
   return (
     <>
       <FlatList
-        ListHeaderComponent={<Picker
-          selectedValue={filter}
-          onValueChange={(itemValue) => {
-            setFilter(itemValue)
-            setselectedRepository(
-              itemValue === "lowest" ? "RATING_AVERAGE" : itemValue
-            );
-            setdirection(itemValue === "lowest" ? "ASC" : "DESC");
-          }}>
-          <Picker.Item label="Latest repositories" value="CREATED_AT" />
-          <Picker.Item label="Highest rated repositories" value="RATING_AVERAGE" />
-          <Picker.Item label="Lowest rated repositories" value="lowest" />
-        </Picker>}
+        ListHeaderComponent={
+          <View>
+            <Searchbar
+              placeholder="Search"
+              onChangeText={setSearchQuery}
+              value={searchQuery}
+              style={{ backgroundColor: "white" }}
+              mode="view"
+            />
+            <Picker
+              selectedValue={filter}
+              onValueChange={(itemValue) => {
+                setFilter(itemValue);
+                setselectedRepository(
+                  itemValue === "lowest" ? "RATING_AVERAGE" : itemValue
+                );
+                setdirection(itemValue === "lowest" ? "ASC" : "DESC");
+              }}
+            >
+              <Picker.Item label="Latest repositories" value="CREATED_AT" />
+              <Picker.Item
+                label="Highest rated repositories"
+                value="RATING_AVERAGE"
+              />
+              <Picker.Item label="Lowest rated repositories" value="lowest" />
+            </Picker>
+          </View>
+        }
       />
       <RepositoryListContainer repositories={repositories} />
     </>
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,9 +1,13 @@
 import { useQuery } from "@apollo/client";
 import { GET_REPOSITORIES } from "../graphql/queries";
 
-const useRepositories = ({selectedRepository, direction}) => {
+const useRepositories = ({ selectedRepository, direction, keyword }) => {
   const { data, error, loading } = useQuery(GET_REPOSITORIES, {
-    variables: { orderBy: selectedRepository, orderDirection: direction},
+    variables: {
+      orderBy: selectedRepository,
+      orderDirection: direction,
+      searchKeyword: keyword,
+    },
     fetchPolicy: "cache-and-network",
   });
   const repositories = data ? data.repositories : null;
